fa5fe8121bd5d8130d38cb1a33a96969e8255829
giorgi-1918 <80577195+giorgi-1918@users.noreply.github.com>
2021-08-10 19:39:30 +0400

    Update StaticVariables.java

--- src/main/java/StaticVariables.java
+++ src/main/java/StaticVariables.java
@@ -1,9 +1,9 @@
 public class StaticVariables {
-    public static int FILL_BLANK_NUM=1;
-    public static int MULTIPLE_CHOICE_NUM=2;
-    public static int PICTURE_RESPONSE_NUM=3;
-    public static int QUESTION_RESPONSE_NUM=4;
-    public static String EMPTY_STRING="";
-    public static String DELIMITER_IN_QUESTION_FIELD_NAME="-";
-    public static String DELIMITER="~";
+    public static final int FILL_BLANK_NUM=1;
+    public static final int MULTIPLE_CHOICE_NUM=2;
+    public static final int PICTURE_RESPONSE_NUM=3;
+    public static final int QUESTION_RESPONSE_NUM=4;
+    public static final String EMPTY_STRING="";
+    public static final String DELIMITER_IN_QUESTION_FIELD_NAME="-";
+    public static final String DELIMITER="~";
 }
